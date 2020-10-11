c55d6417fbcea092b8dbfb5d450a0a47c2234c8a
amitswaraj61 <aswaraj78@gmail.com>
2020-10-11 17:10:04 +0530

    Added code request dispatcher in servlet

--- ServletLearning/src/com/servelets/RegisterServletDemo.java
+++ ServletLearning/src/com/servelets/RegisterServletDemo.java
@@ -3,8 +3,7 @@ package com.servelets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -28,6 +27,7 @@ public class RegisterServletDemo extends HttpServlet{
 		
 		String cond = req.getParameter("condition");
 		
+		if(cond != null) {
 		if(cond.equals("checked")) {
 			out.println("<h2> Name: "+ name + "</h2>");
 			out.println("<h2> Password: "+ pass + "</h2>");
@@ -35,12 +35,24 @@ public class RegisterServletDemo extends HttpServlet{
 			out.println("<h2> Gender: "+ gender + "</h2>");
 			out.println("<h2> Course: "+ course + "</h2>");
 			
+			//forward to SuccessServlet class
+			RequestDispatcher rd = req.getRequestDispatcher("success");
+			rd.forward(req, res);
+			
 		}else {
-			out.println("<h2>You have not accepted terms and condition");
+			out.println("<h2>You have not accepted terms and condition</h2>");
+		}
+    } else {
+    	out.println("<h2>You have not accepted terms and condition</h2>");
+			//I want to include output from index.html
+			
+			//get the object of RequestDispatcher
+			
+			RequestDispatcher rd = req.getRequestDispatcher("index.html");
+			rd.include(req, res);
 		}
 		
-}
-	
-}
+      }
+   }
 	
 
